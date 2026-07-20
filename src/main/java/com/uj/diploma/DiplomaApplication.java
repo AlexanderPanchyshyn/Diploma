baee69f1f9cbdcc50a8531893f303a6826edeeff
Oleksandr Panchyshyn <alexandr.panchyshyn34@gmail.com>
2026-07-21 00:18:13 +0200

    initial commit

---
+++ src/main/java/com/uj/diploma/DiplomaApplication.java
@@ -0,0 +1,13 @@
+package com.uj.diploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uj/diploma/DiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uj.diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
